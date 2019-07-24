a9fe16499f71012293814be5029d3cb8070bf3bb
IzaakWN <iwn_@hotmail.com>
2019-07-24 10:21:17 +0200

    add DM-dependent function; add placeholder for eta-dependent anti-lepton discriminator; disable unused functions

--- python/TauIDSFTool.py
+++ python/TauIDSFTool.py
@@ -1,8 +1,8 @@
 # Author: Izaak Neutelings (July 2019)
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/TauIDRecommendation13TeV
 import os
-from TauPOG.TauIDSFs import modulepath, ensureTFile
-datapath = modulepath+"/../data"
+from TauPOG.TauIDSFs import ensureTFile
+datapath = os.environ['CMSSW_BASE']+"/src/TauPOG/TauIDSFs/data"
 
 class TauIDSFTool:
     
@@ -12,39 +12,68 @@ class TauIDSFTool:
         
         assert year in [2016,2017,2018], "You must choose a year from 2016, 2017, or 2018."
         
-        self.func_sf = { }
-        self.hist_sf = { }
         if id in ['MVAoldDM2017v2']:
           if dm:
-            file = ensureTFile("%s/TauIDSF_dm_%s_%d.root"%(datapath,id,year))
-            self.hist_sf[None]   = file.Get("%s_cent"%(wp))
-            self.hist_sf['Up']   = file.Get("%s_up"%(wp))
-            self.hist_sf['Down'] = file.Get("%s_down"%(wp))
-            self.hist_sf[None].SetDirectory(0)
-            self.hist_sf['Up'].SetDirectory(0)
-            self.hist_sf['Down'].SetDirectory(0)
+            file = ensureTFile("%s/TauID_SF_dm_%s_%d.root"%(datapath,id,year))
+            self.hist = file.Get(wp)
+            self.hist.SetDirectory(0)
             file.Close()
+            self.DMs = [0,1,10]
+            self.getSFvsPT  = self.disabled
+            self.getSFvsEta = self.disabled
           else:
-            file = ensureTFile("%s/TauIDSF_pt_%s_%d.root"%(datapath,id,year))
-            self.func_sf[None]   = file.Get("%s_cent"%(wp))
-            self.func_sf['Up']   = file.Get("%s_up"%(wp))
-            self.func_sf['Down'] = file.Get("%s_down"%(wp))
+            file = ensureTFile("%s/TauID_SF_pt_%s_%d.root"%(datapath,id,year))
+            self.func         = { }
+            self.func[None]   = file.Get("%s_cent"%(wp))
+            self.func['Up']   = file.Get("%s_up"%(wp))
+            self.func['Down'] = file.Get("%s_down"%(wp))
             file.Close()
+            self.getSFvsDM  = self.disabled
+            self.getSFvsEta = self.disabled
+        elif id in ['antiMuonV3','antiEleMVAV6']:
+            raise NotImplementedError("SFs not available yet.")
+            file = ensureTFile("%s/TauID_SF_eta_%s_%d.root"%(datapath,id,year))
+            self.hist = file.Get(wp)
+            self.hist.SetDirectory(0)
+            file.Close()
+            self.genmatches = [2,4] if 'mu' in id.lower() else [1,3]
+            self.getSFvsPT  = self.disabled
+            self.getSFvsDM  = self.disabled
         else:
           raise IOError("Could not recognize tau ID '%s'!"%id)
         
     def getSFvsPT(self, pt, genmatch=5, unc=None):
         """Get tau ID SF vs. tau pT."""
         if genmatch==5:
-          return self.func_sf[unc].Eval(pt)
+          return self.func[unc].Eval(pt)
         else:
-          return 1.
+          return 1.0
         
     def getSFvsDM(self, pt, dm, genmatch=5, unc=None):
-        """Get tau ID SF vs. tau pT."""
-        if genmatch==5 and pt>40:
-          bin = self.hist_sf[unc].GetXaxis().FindBin(dm)
-          return self.hist_sf[unc].GetBinContent(bin)
-        if pt<40:
-          return 0.0
+        """Get tau ID SF vs. tau DM."""
+        if dm in self.DMs:
+          if genmatch==5 and pt>40 and dm in self.DMs:
+            bin = self.hist.GetXaxis().FindBin(dm)
+            if unc=='Up':
+              return self.hist.GetBinContent(bin)+self.hist.GetBinError(bin)
+            elif unc=='Down':
+              return self.hist.GetBinContent(bin)-self.hist.GetBinError(bin)
+            return self.hist.GetBinContent(bin)
+          return 1.0
+        return 0.0
+        
+    def getSFvsEta(self, eta, genmatch, unc=None):
+        """Get tau ID SF vs. tau eta."""
+        if genmatch in self.genmatches:
+          bin = self.hist.GetXaxis().FindBin(eta)
+          if unc=='Up':
+            return self.hist.GetBinContent(bin)+self.hist.GetBinError(bin)
+          elif unc=='Down':
+            return self.hist.GetBinContent(bin)-self.hist.GetBinError(bin)
+          return self.hist.GetBinContent(bin)
+        return 1.0
+        
+    @staticmethod
+    def disabled(*args,**kwargs):
+        raise AttributeError("Disabled method.")
         
