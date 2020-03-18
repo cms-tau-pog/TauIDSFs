f5a22db632f21a8a4243bd2584afd4dcbb206fdb
IzaakWN <iwn_@hotmail.com>
2020-03-18 13:49:52 +0100

    add color

--- test/testTauIDSFTool.py
+++ test/testTauIDSFTool.py
@@ -4,6 +4,7 @@ import time; start0 = time.time()
 from TauPOG.TauIDSFs.TauIDSFTool import TauIDSFTool, TauESTool, TauFESTool
 start1 = time.time()
 
+def green(string,**kwargs): return "\x1b[0;32;40m%s\033[0m"%string
 
 def printSFTable(year,id,wp,vs='pt',emb=False,otherVSlepWP=False):
   assert vs in ['pt','dm','eta'], "'vs' argument should be pt', 'dm' or 'eta'!"
@@ -15,7 +16,7 @@ def printSFTable(year,id,wp,vs='pt',emb=False,otherVSlepWP=False):
   if vs=='pt':
       ptvals = [10,20,21,25,26,30,31,35,40,50,70,100,200,500,600,700,800,1000,1500,2000,]
       print ">>> "
-      print ">>> SF for %s WP of %s in %s"%(wp,id,year)
+      print ">>> SF for %s WP of %s in %s"%(wp,green(id),year)
       print ">>> "
       print ">>> %10s"%('var \ pt')+''.join("%9.1f"%pt for pt in ptvals)
       print ">>> %10s"%("central") +''.join("%9.5f"%sftool.getSFvsPT(pt,5)        for pt in ptvals)
@@ -28,7 +29,7 @@ def printSFTable(year,id,wp,vs='pt',emb=False,otherVSlepWP=False):
     dmvals = [0,1,5,6,10,11]
     for pt in [25,50]:
       print ">>> "
-      print ">>> SF for %s WP of %s in %s with pT = %s GeV"%(wp,id,year,pt)
+      print ">>> SF for %s WP of %s in %s with pT = %s GeV"%(wp,green(id),year,pt)
       print ">>> "
       print ">>> %10s"%('var \ DM')+''.join("%9d"%dm for dm in dmvals)
       print ">>> %10s"%("central") +''.join("%9.5f"%sftool.getSFvsDM(pt,dm,5)        for dm in dmvals)
@@ -44,7 +45,7 @@ def printSFTable(year,id,wp,vs='pt',emb=False,otherVSlepWP=False):
     etavals = [0,0.2,0.5,1.0,1.5,2.0,2.2,2.3,2.4]
     for genmatch in [1,2]:
       print ">>> "
-      print ">>> SF for %s WP of %s in %s with genmatch %d"%(wp,id,year,genmatch)
+      print ">>> SF for %s WP of %s in %s with genmatch %d"%(wp,green(id),year,genmatch)
       print ">>> "
       print ">>> %10s"%('var \ eta')+''.join("%9.3f"%eta for eta in etavals)
       print ">>> %10s"%("central")  +''.join("%9.5f"%sftool.getSFvsEta(eta,genmatch)        for eta in etavals)
@@ -61,7 +62,7 @@ def printTESTable(year,id):
   dmvals  = [0,1,5,10,11]
   for pt in ptvals:
     print ">>> "
-    print ">>> TES for '%s' ('%s') and pT = %s GeV"%(id,year,pt)
+    print ">>> TES for '%s' ('%s') and pT = %s GeV"%(green(id),year,pt)
     print ">>> "
     print ">>> %10s"%('var \ DM')+''.join("%9d"%dm for dm in dmvals)
     print ">>> %10s"%("central") +''.join("%9.5f"%testool.getTES(pt,dm,5)        for dm in dmvals)
@@ -89,17 +90,17 @@ if __name__ == "__main__":
   print ">>> "
   print ">>> start test tau ID SF tool"
   
-  testIDTool   = True and False
+  testIDTool   = True #and False
   testTESTool  = True #and False
-  testFESTool  = True and False
+  testFESTool  = True #and False
   emb          = True and False
   otherVSlepWP = True and False
   
   start2       = time.time()
   years        = [
-    #'2016Legacy',
+    '2016Legacy',
     '2017ReReco',
-    #'2018ReReco',
+    '2018ReReco',
   ]
   tauIDs      = [
     #'MVAoldDM2017v2',
