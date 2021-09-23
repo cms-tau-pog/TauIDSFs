e9db2a927860b288a7563516c6db904b2cf8d661
cardinia <andrea.cardini@desy.de>
2021-09-23 09:19:16 +0200

    Add option for UL in c++ tool

--- src/TauIDSFTool.cc
+++ src/TauIDSFTool.cc
@@ -47,7 +47,7 @@ TauIDSFTool::TauIDSFTool(const std::string& year, const std::string& id, const s
 
   bool verbose = false;
   std::string datapath                = Form("%s/src/TauPOG/TauIDSFs/data",getenv("CMSSW_BASE"));
-  std::vector<std::string> years      = {"2016Legacy","2017ReReco","2018ReReco"};
+  std::vector<std::string> years      = {"2016Legacy","2017ReReco","2018ReReco","UL2016_preVFP","UL2016_postVFP","UL2017","UL2018"};
   std::vector<std::string> antiJetIDs = {"MVAoldDM2017v2","DeepTau2017v2p1VSjet"};
   std::vector<std::string> antiEleIDs = {"antiEleMVA6",   "DeepTau2017v2p1VSe"};
   std::vector<std::string> antiMuIDs  = {"antiMu3",       "DeepTau2017v2p1VSmu"};
